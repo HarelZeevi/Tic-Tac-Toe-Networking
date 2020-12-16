e5c2930517eac06b346be6a775c69eb7ab871da6
Harel Zeevi <62669852+HarelZeevi@users.noreply.github.com>
2020-12-16 14:13:02 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,66 @@
+import socket
+import threading
+
+
+
+
+#the port of communication
+WAIT_MSG = "[SERVER] is waiting for another player..."
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+hostname = socket.gethostbyname(input("Enter server ip address: "))
+port = 1235
+client_socket.connect((hostname, port))
+print(f"You connected to {hostname}")
+msg = ""
+checked_once = False
+
+def receive_msg():
+    received_msg = client_socket.recv(2048)
+    return received_msg
+
+
+def send_msg(msg=""):
+    client_socket.send(bytes(msg, encoding="utf-8"))
+
+send_thread = threading.Thread(target=send_msg)
+send_thread.daemon = True
+send_thread.start()
+
+
+messages = []
+
+def main():
+    run = True
+    global msg
+    while run:
+        msg = receive_msg()
+        if msg:
+            messages.append(msg.decode())
+        else:
+            client_socket.close()
+            run = False
+
+    #creating a thread so you can receive and send messages at the same time.
+    #the sending process will work at the background
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,67 @@
+import socket
+import threading
+import random
+
+#the port of communication
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+hostname = socket.gethostbyname(socket.gethostname())
+port = 1235
+DISCONNECT_MSG = "disconnected!"
+server_socket.bind((hostname, port))
+clients_sockets = []
+server_socket.listen(5)
+print(f"server {hostname}: listening on port {port} ....")
+
+def clients_connections_handler(client_socket, address):
+    global clients_sockets
+    possible_poses = ['11', '21', '31', '12', '22', '32', '13', '23', '33']
+    while True:
+        msg = client_socket.recv(1024)
+        if msg and msg.decode() != DISCONNECT_MSG:
+            print(f"{address}: {msg.decode()} ")
+            for sock in clients_sockets:                
+                if (str(msg.decode())) in possible_poses or (msg.decode() == "you win!") or (msg.decode() == "you lose!") :
+                    if sock != client_socket:
+                        sock.send(msg)
+                        print(f"send: {msg} to:{address}")
+                else:
+                    sock.send(msg)
+                    print(f"send not pos: {msg}")
+                    
+        elif msg.decode():
+            clients_sockets[0].close()
+            clients_sockets[1].close()
+            break
+
+
+def x_or_o():
+    options = ["x", "o"]
+    first_rand_choice = random.choice(options)
+    options.remove(first_rand_choice)
+    second_rand_choice = "".join(options)
+    return first_rand_choice, second_rand_choice
+
+
+while True:
+    client_socket, address = server_socket.accept()
+    print(f"{address[1]}: is connected...")
+    clients_sockets.append(client_socket)
+    if len(clients_sockets) == 2:
+        first_rand_choice, second_rand_choice = x_or_o()
+        clients_sockets[0].send(bytes(first_rand_choice, encoding="utf-8"))
+        clients_sockets[1].send(bytes(second_rand_choice, encoding="utf-8"))
+    else:
+        client_socket.send(bytes("[SERVER] is waiting for another player...", encoding="utf-8"))
+
+
+
+    #creaing a thread for each client_socket connection so many clients will be able to connect at once
+    client_thread = threading.Thread(target = clients_connections_handler, args=(client_socket, address))
+    #setting a parameter that allows to stop running the program even a thread is still processing
+    client_thread.daemon = True
+    #starting the thread
+    client_thread.start()
+
+
+
+
